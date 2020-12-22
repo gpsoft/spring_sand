a20d84e5f1d2dc42e559fd4f84f2672ed1a5d4d9
gpsoft <etoh.gpsoft@gmail.com>
2020-12-22 22:55:05 +0900

    Add river model

---
+++ src/main/java/jp/dip/gpsoft/springsand/model/River.java
@@ -0,0 +1,67 @@
+package jp.dip.gpsoft.springsand.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "rivers")
+public class River {
+	@Id
+	@GeneratedValue
+	private Integer id;
+
+	private String name;
+	private String source;
+	private String mouse;
+
+	public River() {
+	}
+
+	public River(String name, String source, String mouse) {
+		this.name = name;
+		this.source = source;
+		this.mouse = mouse;
+	}
+
+	@Override
+	public String toString() {
+		return "River#" + id + "[name=" + name + ", from " + source + " to " + mouse
+				+ "]";
+	}
+
+	// ------------- generated getters and setters
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getMouse() {
+		return mouse;
+	}
+
+	public void setMouse(String mouse) {
+		this.mouse = mouse;
+	}
+}
---
+++ src/main/java/jp/dip/gpsoft/springsand/repository/RiverRepository.java
@@ -0,0 +1,9 @@
+package jp.dip.gpsoft.springsand.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jp.dip.gpsoft.springsand.model.River;
+
+public interface RiverRepository extends JpaRepository<River, Integer> {
+
+}
