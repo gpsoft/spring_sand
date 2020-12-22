ebb8c0841b74b7be6f0173a87c01ee63266c974a
gpsoft <etoh.gpsoft@gmail.com>
2020-12-22 21:00:26 +0900

    Start project

---
+++ src/main/java/jp/dip/gpsoft/SpringSandApplication.java
@@ -0,0 +1,13 @@
+package jp.dip.gpsoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSandApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSandApplication.class, args);
+	}
+
+}
