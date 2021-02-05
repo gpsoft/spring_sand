8515e923528d8c8087f007f6571aecc5e742a73b
gpsoft <etoh.gpsoft@gmail.com>
2021-02-05 23:15:58 +0900

    Add test sample#2
    Using Jsoup as html parser

---
+++ src/test/java/jp/dip/gpsoft/springsand/HttpRequestTests.java
@@ -0,0 +1,50 @@
+package jp.dip.gpsoft.springsand;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+/**
+ * テストサンプル2
+ * 
+ * - 適当なポートでサーバを起動
+ * - GETリクエストを投げて、レスポンスHTMLの内容を検証
+ */
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class HttpRequestTests {
+
+	@LocalServerPort
+	private int port;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	private String url(String path) {
+		return "http://localhost:" + port + path;
+	}
+
+	private String get(String path) {
+		return restTemplate.getForObject(url(path), String.class);
+	}
+
+	@Test
+	public void landingPage() throws Exception {
+		Document doc = Jsoup.parse(get("/"));
+
+		assertThat(doc.select(".user-info").text()).as("ログインボタン").isEqualTo("Login");
+		assertThat(doc.select("h3").text()).as("見出し").isEqualTo("Menu");
+		assertThat(doc.select("ul.menu li").size()).as("メニューアイテム数").isEqualTo(4);
+		assertThat(doc.select("ul.menu").text()).as("メニュー")
+				.contains("CRUD of Rivers")
+				.contains("CRUD of Lakes")
+				.contains("CRUD of Valleys")
+				.contains("CRUD of Users");
+	}
+}
